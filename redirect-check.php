44d01439347c140a5be7adefa5a24a13a20637d0
Tim Mullen <tmullen@zetaglobal.com>
2017-09-26 09:25:28 -0500

    sync

--- redirect-check.php
+++ redirect-check.php
@@ -4,21 +4,23 @@ $url = $_GET['url'];
 function checkurl($theurl) {
   $cookie = tempnam ("/tmp", "CURLCOOKIE");
   $ch = curl_init();
-  curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1");
+  curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36");
   curl_setopt($ch, CURLOPT_URL, $theurl);
   curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie);
-  curl_setopt($ch, CURLOPT_ENCODING, "");
-  curl_setopt($ch, CURLOPT_HEADER, true);
+  curl_setopt($ch, CURLOPT_ENCODING, "UTF-8");
+  curl_setopt($ch, CURLOPT_HEADER, 0);
   curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: */*'));
   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-  curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_REFERER, $theurl);
+  curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
   curl_setopt($ch, CURLOPT_MAXREDIRS, 4);
-  curl_setopt($ch, CURLOPT_NOBODY, true);
+  curl_setopt($ch, CURLOPT_NOBODY, false);
   $content = curl_exec($ch);
   $response = curl_getinfo($ch);
   curl_close($ch);
@@ -39,4 +41,4 @@ while ($i <= 4) {
 }
 
 echo json_encode($dataout);
- ?>
+?>
